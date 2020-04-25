d56ed704c6677dde7d04b3c8aa6990c349d7c22c
haoclay <51101911+haoclay@users.noreply.github.com>
2020-04-25 11:54:22 +0800

    就这

---
+++ src/main/java/cn/enjoy/model/Dog.java
@@ -0,0 +1,8 @@
+package cn.enjoy.model;
+
+/**
+ * Created by NAGOYA on 2020/4/25.
+ */
+public class Dog {
+   private String name;
+}
---
+++ src/main/java/cn/enjoy/model/Person.java
@@ -0,0 +1,26 @@
+package cn.enjoy.model;
+
+/**
+ * Created by NAGOYA on 2020/4/25.
+ * @author sgq
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
